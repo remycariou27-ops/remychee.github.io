443838d0e6110dcbac82d7611c4dfbfbb751365c
remycariou27-ops <remycariou27@gmail.com>
2026-06-22 18:19:56 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -12,6 +12,6 @@
    N'utilisez JAMAIS la clé "service_role" ici.
    ==================================================================== */
 window.NORDHAVEN_CONFIG = {
-  SUPABASE_URL: "VOTRE_PROJECT_URL_ICI",
-  SUPABASE_ANON_KEY: "VOTRE_CLE_ANON_ICI",
+  SUPABASE_URL: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cG5wamx5dHR1aGNvenppenVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NjU3NzMsImV4cCI6MjA5NzA0MTc3M30.w0MxDSrn7MhHVdGRn3Lc9hjU3h74uteXqvXknLaON4k",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cG5wamx5dHR1aGNvenppenVzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTQ2NTc3MywiZXhwIjoyMDk3MDQxNzczfQ.2aisdLeFfbDwea5qYbBroeGVS1FlZmx1A6FlgUinnGw",
 };
